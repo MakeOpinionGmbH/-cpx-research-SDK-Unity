b94b8a367778930c84afe8c6b62c8cd0b3d1032f
Daniel Fredrich <MrBugBears@users.noreply.github.com>
2021-03-31 17:12:29 +0200

    Initial upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# CPX Research Android SDK
+
+#### Monetize your product with fun surveys.
+
+We will make it easy for you: Simply implement our solution and be ready to start monetizing your product immediately!
+Let users earn your virtual currency by simply participating in exciting and well paid online surveys!
+
+This SDK is owned by [MakeOpinion GmbH](http://www.makeopinion.com).
+
+[Learn more.](https://cpx-research.com/)
+
+# Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+
+# Prerequisites
+
+- .Net 2.0 compatible Unity project
+
+# Installation
+
+1. Download the latest release.
+
+2. Copy the contents of the prefabs directory to your prefab directory in the project.
+
+3. Copy the content of the plugins folder to your plugins folder in your project.
+
+# Usage
+
+## Using the prefabs
+
+Drag one of the prefabs on your scene and position it according to your needs. The prefabs are pre-configured with the script for using CPX Research. All you need to add are you app id, external user id and the secure hash.
+
+## Using the CPXResearchScript
+
+You can drop the CPXResearchScript on any game object you like. If you do so you need to connect an image to the script property "Image for content". Also you need to add your app id, external user id and the secure hash.
---
+++ plugins/CPXResearchScript.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using CPXResearch;
+
+public class CPXResearchScript : MonoBehaviour, IPointerClickHandler, ICPXResearch
+{
+    [Header("Banner")]
+    public Image imageForContent;
+
+    [Header("Style")]
+    public CPXStyleConfiguration.SurveyPosition position;
+    public string bannerText = "Take a survey";
+    public int bannerTextSize = 20;
+    public Color bannerTextColor;
+    public Color bannerBgColor;
+    public bool roundedCorners = true;
+
+    [Header("Configuration")]
+    public string appId;
+    public string extUserId;
+    public string secureHash;
+
+    private CPXResearch.CPXResearch cpx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CPXStyleConfiguration Style = new CPXStyleConfiguration(position,
+            bannerText,
+            bannerTextSize,
+            ColorUtility.ToHtmlStringRGB(bannerTextColor),
+            ColorUtility.ToHtmlStringRGB(bannerBgColor),
+            true);
+        CPXConfiguration Config = new CPXConfiguration(appId, extUserId, secureHash, Style);
+
+        cpx = new CPXResearch.CPXResearch(Config)
+        {
+            Callback = this
+        };
+        cpx.SetSurveyVisible(true);
+        StartCoroutine(cpx.RequestSurveyUpdate(false));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.OpenURL(cpx.GetSurveysListUrl());
+    }
+
+    // ICPXResearch impl
+    void ICPXResearch.OnSurveysUpdated()
+    {
+        Debug.Log($"Surveys received.");
+        StartCoroutine(cpx.GetImage(texture =>
+        {
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+
+            if (sprite != null)
+            {
+                imageForContent.sprite = sprite;
+            }
+        }, error =>
+        {
+            Debug.LogError(error);
+        }));
+    }
+
+    void ICPXResearch.OnTransactionsUpdated(CPXResearchLib.TransactionItem[] UnpaidTransactions)
+    {
+        foreach (CPXResearchLib.TransactionItem item in cpx.UnpaidTransactions)
+        {
+            Debug.Log($"Transaction: {item.TransactionId}");
+        }
+    } 
+
+    void ICPXResearch.OnSurveysDidOpen()
+    {
+        Debug.Log("Did open Surveys.");
+    }
+
+    void ICPXResearch.OnSurveysDidClose()
+    {
+        Debug.Log("Did close Surveys.");
+    }
+}
